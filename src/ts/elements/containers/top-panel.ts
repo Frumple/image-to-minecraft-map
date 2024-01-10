957e19cf9e4da42c288557441032ac4d71311721
Frumple <frumple1@gmail.com>
2024-01-09 19:57:23 -0800

    Add version to top bar.

--- src/ts/elements/containers/top-panel.ts
+++ src/ts/elements/containers/top-panel.ts
@@ -1,13 +1,19 @@
 import BaseContainer from '@elements/containers/base-container';
 
+import CurrentContext from '@models/current-context';
+
 export default class TopPanel extends BaseContainer {
   static get elementName() { return 'top-panel'; }
 
+  versionText: HTMLSpanElement;
+
   constructor() {
     super();
+
+    this.versionText = this.getShadowElement('version-text') as HTMLSpanElement;
   }
 
   initialize() {
-
+    this.versionText.textContent = `v${CurrentContext.settings.appVersion}`;
   }
 }
